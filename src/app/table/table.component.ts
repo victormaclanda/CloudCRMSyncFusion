3dc64fdd21eded350bf60700e77cc8e43b4b1d18
Cesar <cesargupe95@gmail.com>
2020-09-08 10:06:08 +0200

    customize table

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,24 +1,26 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
-import { GridComponent } from '@syncfusion/ej2-angular-grids';
+import { SelectionService } from '@syncfusion/ej2-angular-grids';
 
 const SERVICE_URI: string = 'https://ej2services.syncfusion.com/production/web-services/';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
+  providers: [SelectionService]
 })
 export class TableComponent implements OnInit {
-
   public data: DataManager;
   public pageSettings: Object;
-
-  @ViewChild('grid')
-  public grid: GridComponent;
+  public selectOptions: Object;
+  public editSettings: Object;
+  public toolbar: string[];
 
   ngOnInit(): void {
-      this.data = new DataManager({ url: SERVICE_URI + 'api/Orders', adaptor: new WebApiAdaptor });
-      this.pageSettings = { pageCount: 3 };
+    this.data = new DataManager({ url: SERVICE_URI + 'api/Orders', adaptor: new WebApiAdaptor() });
+    this.pageSettings = { pageCount: 3 };
+    this.selectOptions = { persistSelection: true };
+    this.editSettings = { allowDeleting: true };
   }
 }
